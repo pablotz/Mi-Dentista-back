15e7fdea443a48d67c8a2674e5c6f3e967fc8817
Villegas Marco <villegascmarco@gmail.com>
2021-11-07 16:30:14 -0600

    Alter system_user table: FK access_code was added

--- app_main/core/model/system_user.py
+++ app_main/core/model/system_user.py
@@ -2,6 +2,8 @@ from sqlalchemy.sql.expression import true
 from app_main.connection import db
 from sqlalchemy.sql import func
 
+from app_main.core.model.access_code import access_code
+
 
 class system_user(db.Model):
 
@@ -13,5 +15,7 @@ class system_user(db.Model):
     user_password = db.Column(db.String(88), nullable=False)
     user_status = db.Column(db.Integer, nullable=False, default=1)
     user_role = db.Column(db.Integer, nullable=False)
+    access_code = db.Column(db.String(8), db.ForeignKey(
+        access_code.id), nullable=true,)
     created_at = db.Column(db.DateTime(timezone=True),
                            server_default=func.now(), nullable=False)
