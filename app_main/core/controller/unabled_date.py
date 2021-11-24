c9ecd1c3c5490383db31ca894b58721951b30ee8
Villegas Marco <villegascmarco@gmail.com>
2021-11-23 21:59:07 -0600

    unabled date module was done

--- app_main/core/controller/unabled_date.py
+++ app_main/core/controller/unabled_date.py
@@ -1,4 +1,6 @@
 import datetime
+from ...connection import db
+from sqlalchemy.sql.expression import true
 from ..model.unabled_date import unabled_date as model
 
 
@@ -9,47 +11,65 @@ def add(request, user_id):
     request_json = request.json
 
     date = get_or_error(request_json, 'date')
+    validate_is_unique_date(date)
 
-    repetitive = get(request_json, 'repetitive')
+    if type(date) is list:
+        for day in date:
+            unabled_date = model(date=day, created_by=user_id)
+            db.session.add(unabled_date)
+    else:
+        unabled_date = model(date=date, created_by=user_id)
+        db.session.add(unabled_date)
 
-    if repetitive:
-        # Must be a weekday
-        validate_weekday(date)
-
-    all_day = get(request_json, 'all_day')
-    if not all_day:
-        # Must have a start and end hour
-        start_hour = get_or_error(request_json, 'start_hour')
-        start_hour = datetime.datetime.strptime(start_hour, '%H:%M').time()
-        end_hour = get_or_error(request_json, 'end_hour')
-        end_hour = datetime.datetime.strptime(end_hour, '%H:%M').time()
-        print(start_hour, end_hour)
-
-    return get_all()
+    db.session.add(unabled_date)
+    db.session.commit()
+    return "Ok"
 
 
 def get_all():
-    return model.query.all()
+    # Get all unabled_date sorted by date
+    unabled_dates = model.query.order_by(model.date).all()
+
+    dic = [unabled_date.__dict__ for unabled_date in unabled_dates]
+    for i in range(len(dic)):
+        del dic[i]['_sa_instance_state']
+        del dic[i]['created_at']
+        del dic[i]['created_by']
+    return dic
 
 
-def validate_weekday(weekday):
-    week_list = ['monday', 'tuesday', 'wednesday',
-                 'thursday', 'friday', 'saturday', 'sunday']
-    if type(weekday) is list:
-        for day in weekday:
-            if day.lower() not in week_list:
-                raise Exception(f"{day} is not a valid weekday")
-        return
-    if weekday.lower() not in week_list:
-        raise Exception(f"{weekday} is not a valid weekday")
+def delete(date):
+    unabled_date = model.query.filter(model.date == date).first()
+    db.session.delete(unabled_date)
+    db.session.commit()
+    return "Ok"
 
 
-def get(json, attribute):
+def search(id):
+    unabled_date = model.query.filter(model.id == id).first()
+
+    dic = unabled_date.__dict__
+    del dic['_sa_instance_state']
+    return dic
+
+
+def is_date(date):
     try:
-        return json[attribute]
-
+        datetime.datetime.strptime(date, '%Y-%m-%d')
     except Exception:
-        return None
+        raise Exception(f"{date} is not a valid date")
+
+
+def validate_is_unique_date(date):
+    if type(date) is list:
+        for day in date:
+            is_date(day)
+            if model.query.filter(model.date == day).first():
+                raise Exception(f"{day} is already in use")
+        return
+    is_date(date)
+    if model.query.filter(model.date == date).first():
+        raise Exception(f"{date} is already in use")
 
 
 def get_or_error(json, attribute):
--- app_main/core/model/unabled_date.py
+++ app_main/core/model/unabled_date.py
@@ -8,9 +8,5 @@ from app_main.core.model.system_user import system_user
 class unabled_date(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     date = db.Column(db.Date)
-    weekday = db.Column(db.Integer)
-    all_day = db.Column(db.Boolean, default=False)
-    start_hour = db.Column(db.Time)
-    end_hour = db.Column(db.Time)
     created_at = db.Column(db.DateTime, default=func.now())
     created_by = db.Column(db.Integer, db.ForeignKey(system_user.id))
